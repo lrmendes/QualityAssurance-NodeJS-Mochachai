5f32e0cd1b757f682897407a7fca3f16d1179003
lrmendes <lrmen14@gmail.com>
2021-01-02 22:14:55 +0000

    ADD -> Solved Exercises Step4 to Step 7

---
+++ SOLVED-EXERCISES/Step7/1_unit-tests.js
@@ -0,0 +1,55 @@
+const chai = require('chai');
+const assert = chai.assert;
+
+suite('Unit Tests', function () {
+  suite('Basic Assertions', function () {
+    // #1
+    test('#isNull, #isNotNull', function () {
+      assert.isNull(null, 'this is an optional error description - e.g. null is null');
+      assert.isNotNull(1, '1 is not null');
+    });
+    // #2
+    test('#isDefined, #isUndefined', function () {
+      assert.isDefined(null, 'null is not undefined');
+      assert.isUndefined(undefined, 'undefined IS undefined');
+      assert.isDefined('hello', 'a string is not undefined');
+    });
+    // #3
+    test('#isOk, #isNotOk', function () {
+      assert.isNotOk(null, 'null is falsey');
+      assert.isOk("I'm truthy", 'a string is truthy');
+      assert.isOk(true, 'true is truthy');
+    });
+    // #4
+    test('#isTrue, #isNotTrue', function () {
+      assert.isTrue(true, 'true is true');
+      assert.isTrue(!!'double negation', 'double negation of a truthy is true');
+      assert.isNotTrue({ value: 'truthy' }, 'A truthy object is NOT TRUE (neither is false...)');
+    });
+  });
+
+  // -----------------------------------------------------------------------------
+
+  suite('Equality', function () {
+    // #5
+    test('#equal, #notEqual', function () {
+      assert.equal(12, '12', 'numbers are coerced into strings with == ');
+      assert.notEqual({ value: 1 }, { value: 1 }, '== compares object references');
+      assert.equal(6 * '2', '12', 'no more hints...');
+      assert.notEqual(6 + '2', '12', 'type your error message if you want');
+    });
+    // #6
+    test('#strictEqual, #notStrictEqual', function () {
+      assert.notStrictEqual(6, '6');
+      assert.strictEqual(6, 3 * 2);
+      assert.strictEqual(6 * '2', 12);
+      assert.notStrictEqual([1, 'a', {}], [1, 'a', {}]);
+    });
+    // #7
+    test('#deepEqual, #notDeepEqual', function () {
+      assert.deepEqual({ a: '1', b: 5 }, { b: 5, a: '1' }, "keys order doesn't matter");
+      assert.notDeepEqual({ a: [5, 6] }, { a: [6, 5] }, "array elements position does matter !!");
+    });
+  });
+
+});
